c0ff749d6393574a1e753944faa24fe8ff5876b2
Kiwi-7 <60024260+Kiwi-7@users.noreply.github.com>
2023-12-05 21:59:23 +0700

    All files
    All primary function finished and worked

---
+++ MineSweeper/src/main/java/com/mycompany/minesweeper/MineSweeper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minesweeper;
+
+/**
+ *
+ * @author DELL
+ */
+public class MineSweeper {
+
+    public static void main(String[] args) throws Exception{
+        GameFrame gframe = new GameFrame(12,22,75);
+    }
+}
